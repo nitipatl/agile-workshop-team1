a950c05cba7fddd2b6daec36640bc6f212aa2e21
ITC-37-179\Administrator <little.rainn@gmail.com>
2017-07-14 14:48:37 +0700

    commit

--- Coding/Cupid/CheckLogin.php
+++ Coding/Cupid/CheckLogin.php
@@ -7,8 +7,8 @@ $dbname = "team1";
 $mysqli = new MySQLI($host,$username,$password,$dbname);
 $result = $mysqli->query(" select  * from user  WHERE  user='$_POST[user]' AND password='$_POST[password]' ") or die(mysqli_error());;   
 
-if($result[id]<>''){
-	$_SESSION['id'] = $result[id];
+if($result[id]!=''){
+	$_SESSION['id'] = $result['id'];
 	include('main.php');
 };
 ?>
